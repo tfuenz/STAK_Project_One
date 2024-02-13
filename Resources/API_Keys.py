e0126597055213f05d1d78cf0d0803511260279a
kgregart <153472472+kgregart@users.noreply.github.com>
2024-02-12 20:51:25 -0500

    API Keys

---
+++ Resources/API_Keys.py
@@ -0,0 +1,8 @@
+# Pet Finder API Key
+pet_api_key = "eSn1OG1UeBmhKYojYVewFjUtePuf4RS6M5lccGy2VZA1cN8tKY"
+
+# Pet Finder API Key Secret
+secret_api = "MGuD8b762zAY9RGisSDEwGGr2nfNEVfpeQkR7RDs"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
